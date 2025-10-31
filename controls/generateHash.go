c1afea9963ddc28f89bd1c70625febaef6f72888
mestredainfo <murilogomes5792@gmail.com>
2025-10-31 15:28:56 -0300

    Added header text

--- controls/generateHash.go
+++ controls/generateHash.go
@@ -1,3 +1,8 @@
+// MiCheckHash
+// Copyright (C) 2025 Murilo Gomes Julio. Todos os direitos reservados.
+
+// Este software e código-fonte é distribuído sob os termos do Contrato de Licença de Usuário Final do MiCheckHash.
+
 package controls
 
 import (
